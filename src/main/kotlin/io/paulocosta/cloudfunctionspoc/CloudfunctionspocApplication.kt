70eef10f76e8a262eea9d9497c5d6089583aeabb
Paulo Costa <paulombcosta@gmail.com>
2019-03-17 17:03:57 -0300

    I'm getting somewhere

--- src/main/kotlin/io/paulocosta/cloudfunctionspoc/CloudfunctionspocApplication.kt
+++ src/main/kotlin/io/paulocosta/cloudfunctionspoc/CloudfunctionspocApplication.kt
@@ -1,11 +1,23 @@
 package io.paulocosta.cloudfunctionspoc
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CloudfunctionspocApplication
+class CloudfunctionspocApplication {
 
-fun main(args: Array<String>) {
-    runApplication<CloudfunctionspocApplication>(*args)
+    @Bean
+    open fun test(): () -> String {
+        return { "hai" }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(CloudfunctionspocApplication::class.java, *args)
+        }
+    }
+
 }
+
