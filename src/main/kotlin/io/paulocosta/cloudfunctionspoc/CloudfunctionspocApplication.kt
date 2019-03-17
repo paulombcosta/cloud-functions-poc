8ac01d3187fdb207aac5442c1c1540bea78a533d
Paulo Costa <paulombcosta@gmail.com>
2019-03-17 13:55:39 -0300

    Initial commit

---
+++ src/main/kotlin/io/paulocosta/cloudfunctionspoc/CloudfunctionspocApplication.kt
@@ -0,0 +1,11 @@
+package io.paulocosta.cloudfunctionspoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudfunctionspocApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudfunctionspocApplication>(*args)
+}
---
+++ src/test/kotlin/io/paulocosta/cloudfunctionspoc/CloudfunctionspocApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.paulocosta.cloudfunctionspoc
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CloudfunctionspocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
