089d1cb8f80ddaf3f9a207868327b13eb1258da5
Paulo Costa <paulombcosta@gmail.com>
2019-04-01 01:18:33 -0300

    WIP

--- src/main/kotlin/io/paulocosta/cloudfunctionpoc/Handler.kt
+++ src/main/kotlin/io/paulocosta/cloudfunctionpoc/Handler.kt
@@ -1,18 +1,12 @@
 package io.paulocosta.cloudfunctionpoc
 
-import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler
-import org.springframework.stereotype.Component
 
 //@Component
 //class Handler : SpringBootApiGatewayRequestHandler() {
 //
-//    override fun handleRequest(event: APIGatewayProxyRequestEvent?, context: Context?): Any {
-//    }
-
-//    @Bean
 //    fun test(): () -> String {
 //    }
-
+//
 //}
+class Handler : SpringBootApiGatewayRequestHandler()
