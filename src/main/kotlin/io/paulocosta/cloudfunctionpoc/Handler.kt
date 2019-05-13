2728a4efc3994ad53994e5f4b382cc32fa4c2675
Paulo Costa <paulombcosta@gmail.com>
2019-05-13 17:08:30 -0300

    cleanup

--- src/main/kotlin/io/paulocosta/cloudfunctionpoc/Handler.kt
+++
@@ -1,7 +0,0 @@
-package io.paulocosta.cloudfunctionpoc
-
-import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler
-import org.springframework.stereotype.Component
-
-@Component
-class Handler : SpringBootApiGatewayRequestHandler()
